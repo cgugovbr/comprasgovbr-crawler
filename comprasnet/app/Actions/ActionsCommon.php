11ebb799d625f7d48c50c58f30c522529b0357b1
Evert Ramos <evert.ramos@cgu.gov.br>
2023-07-31 13:33:38 -0300

    Criar ação comum para duplicidae de resposta na API

---
+++ comprasnet/app/Actions/ActionsCommon.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Comprasnet\App\Actions;
+
+use Illuminate\Support\Arr;
+
+class ActionsCommon {
+
+    /**
+     * Helper para extrai dados de um array
+     *
+     * @param $array
+     * @param ...$keys
+     * @return string|null
+     */
+    public static function validaDataArray($array, ...$keys) : string|null
+    {
+        foreach ($keys as $key) {
+            $array_dot = Arr::dot($array);
+            if (isset($array_dot[$key]) && is_string($array_dot[$key]) && $array_dot[$key] <> '') {
+                return $array_dot[$key];
+            }
+        }
+
+        return null;
+    }
+}
--- comprasnet/app/Actions/AdicionarContrato.php
+++ comprasnet/app/Actions/AdicionarContrato.php
@@ -3,11 +3,12 @@
 namespace Comprasnet\App\Actions;
 
 use Comprasnet\App\Models\Contrato;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 use Comprasnet\App\Mail\ErroImportacao;
 
-class AdicionarContrato {
+class AdicionarContrato extends ActionsCommon {
 
     /**
      * Adiciona/Atualiza um Contrato
@@ -20,6 +21,7 @@ class AdicionarContrato {
     public static function addContrato($data, $command = null) : void
     {
         try {
+
             $contrato = Contrato::firstOrNew(['IdContrato' => $data['id']]);
 
             $contrato->IdContrato = $data['id'];
@@ -27,43 +29,36 @@ class AdicionarContrato {
             $contrato->NumContrato = (isset($data['numero' ]) && $data['numero'] <> '') ? $data['numero'] : null;
 
             // Contratante
-            $contrato->CodOrgaoContratante = (isset($data['contratante']['orgao']['codigo'])) ? $data['contratante']['orgao']['codigo'] : null;
-            $contrato->NomOrgaoContratante = (isset($data['contratante']['orgao']['nome'])) ? $data['contratante']['orgao']['nome'] : null;
-            $contrato->CodUnidadeGestoraContratante = (isset($data['contratante']['orgao']['unidade_gestora']['codigo'])) ? $data['contratante']['orgao']['unidade_gestora']['codigo'] : null;
-            $contrato->ResNomeUnidadeGestoraContratante = (isset($data['contratante']['orgao']['unidade_gestora']['nome_resumido'])) ? $data['contratante']['orgao']['unidade_gestora']['nome_resumido'] : null;
-            $contrato->NomUnidadeGestoraContratante = (isset($data['contratante']['orgao']['unidade_gestora']['nome'])) ? $data['contratante']['orgao']['unidade_gestora']['nome'] : null;
+            $contrato->CodOrgaoContratante = ActionsCommon::validaDataArray($data, 'contratante.orgao.codigo', 'orgao_codigo');
+            $contrato->NomOrgaoContratante = ActionsCommon::validaDataArray($data, 'contratante.orgao.nome', 'orgao_nome');
+            $contrato->CodUnidadeGestoraContratante = ActionsCommon::validaDataArray($data, 'contratante.orgao.unidade_gestora.codigo', 'unidade_codigo');
+            $contrato->ResNomeUnidadeGestoraContratante = ActionsCommon::validaDataArray($data, 'contratante.orgao.unidade_gestora.nome_resumido', 'unidade_nome_resumido');
+            $contrato->NomUnidadeGestoraContratante = ActionsCommon::validaDataArray($data, 'contratante.orgao.unidade_gestora.nome', 'unidade_nome');
 
             // Fornecedor
-            $contrato->TpFornecedor = (isset($data['fornecedor']['tipo']) && $data['fornecedor']['tipo'] <> '') ? $data['fornecedor']['tipo'] : null;
-            $contrato->NumCnpjCpf = (isset($data['fornecedor']['cnpj_cpf_idgener']) && $data['fornecedor']['cnpj_cpf_idgener'] <> '') ? str_replace(['.', '/', '-'], ['', '', ''], $data['fornecedor']['cnpj_cpf_idgener']) : null;
-            $contrato->NomFornecedor = (isset($data['fornecedor']['nome']) && $data['fornecedor']['nome'] <> '') ? $data['fornecedor']['nome'] : null;
+            $contrato->TpFornecedor = ActionsCommon::validaDataArray($data, 'fornecedor.tipo', 'fornecedor_tipo');
+            $contrato->NumCnpjCpf = str_replace(['.', '/', '-'], ['', '', ''], ActionsCommon::validaDataArray($data, 'fornecedor.cnpj_cpf_idgener', 'fonecedor_cnpj_cpf_idgener'));
+            $contrato->NomFornecedor = ActionsCommon::validaDataArray($data, 'fornecedor.nome', 'fornecedor_nome');
 
-            $contrato->TpContrato = (isset($data['tipo']) && $data['tipo'] <> '') ? $data['tipo'] : null;
-            $contrato->SitContrato = (isset($data['situacao' ]) && $data['situacao'] <> '') ? $data['situacao'] : null;
-            $contrato->CatContrato = (isset($data['categoria']) && $data['categoria'] <> '') ? $data['categoria'] : null;
-            $contrato->TxtSubcategoria = (isset($data['subcategoria']) && $data['subcategoria'] <> '') ? $data['subcategoria'] : null;
-            $contrato->NomUnidadesRequisitantes = (isset($data['unidades_requisitantes']) && $data['unidades_requisitantes'] <> '') ? $data['unidades_requisitantes'] : null;
-            $contrato->NumProcesso = (isset($data['processo']) && $data['processo'] <> '') ? $data['processo'] : null;
-            $contrato->DescObjeto = (isset($data['objeto']) && $data['objeto'] <> '') ? $data['objeto'] : null;
-            $contrato->TxtInformacaoComplementar = (isset($data['informacao_complementar']) && $data['informacao_complementar'] <> '') ? $data['informacao_complementar'] : null;
-            $contrato->DescModalidade = (isset($data['modalidade']) && $data['modalidade'] <> '') ? $data['modalidade'] : null;
-            $contrato->NumLicitacao = (isset($data['licitacao_numero']) && $data['licitacao_numero'] <> '') ? $data['licitacao_numero'] : null;
-            $contrato->DatAssinatura = (isset($data['data_assinatura']) && $data['data_assinatura'] <> '') ? $data['data_assinatura'] : null;
-            $contrato->DatPublicacao = (isset($data['data_publicacao']) && $data['data_publicacao'] <> '') ? $data['data_publicacao'] : null;
-            $contrato->DatVigenciaInicio = (isset($data['data_publicacao']) && $data['data_publicacao'] <> '') ? $data['data_publicacao'] : null;
-            $contrato->DatVigenciaFim = (isset($data['vigencia_fim']) && $data['vigencia_fim'] <> '') ? $data['vigencia_fim'] : null;
-            $contrato->ValInicial = (isset($data['valor_inicial']) && $data['valor_inicial'] <> '') ? str_replace(['.', ','], ['', '.'], $data['valor_inicial']) : null;
-            $contrato->ValGlobal = (isset($data['valor_global']) && $data['valor_global'] <> '') ? str_replace(['.', ','], ['', '.'], $data['valor_global']) : null;
-            $contrato->NumParcelas = (isset($data['num_parcelas']) && $data['num_parcelas'] <> '') ? $data['num_parcelas'] : null;
-            $contrato->ValParcela = (isset($data['valor_parcela']) && $data['valor_parcela'] <> '') ? str_replace(['.', ','], ['', '.'], $data['valor_parcela']) : null;
-            $contrato->ValAcumulado = (isset($data['valor_acumulado']) && $data['valor_acumulado'] <> '') ? str_replace(['.', ','], ['', '.'], $data['valor_acumulado']) : null;
-            $contrato->EndLinkEmpenhos = (isset($data['links']['empenhos']) && $data['links']['empenhos'] <> '') ? $data['links']['empenhos'] : null;
-            $contrato->EndLinkCronograma = (isset($data['links']['cronograma']) && $data['links']['cronograma'] <> '') ? $data['links']['cronograma'] : null;
-            $contrato->EndLinkHistorico = (isset($data['links']['historico']) && $data['links']['historico'] <> '') ? $data['links']['historico'] : null;
-            $contrato->EndLinkPrepostos = (isset($data['links']['prepostos']) && $data['links']['prepostos'] <> '') ? $data['links']['prepostos'] : null;
-            $contrato->EndLinkFaturas = (isset($data['links']['faturas']) && $data['links']['faturas'] <> '') ? $data['links']['faturas'] : null;
-            $contrato->EndLinkResponsaveis = (isset($data['links']['responsaveis']) && $data['links']['responsaveis'] <> '') ? $data['links']['responsaveis'] : null;
-            $contrato->EndLinkArquivos = (isset($data['links']['arquivos']) && $data['links']['arquivos'] <> '') ? $data['links']['arquivos'] : null;
+            $contrato->TpContrato = ActionsCommon::validaDataArray($data,'tipo');
+            $contrato->SitContrato = ActionsCommon::validaDataArray($data,'situacao');
+            $contrato->CatContrato = ActionsCommon::validaDataArray($data,'categoria');
+            $contrato->TxtSubcategoria = ActionsCommon::validaDataArray($data,'subcategoria');
+            $contrato->NomUnidadesRequisitantes = ActionsCommon::validaDataArray($data,'unidades_requisitantes');
+            $contrato->NumProcesso = ActionsCommon::validaDataArray($data,'processo');
+            $contrato->DescObjeto = ActionsCommon::validaDataArray($data,'objeto');
+            $contrato->TxtInformacaoComplementar = ActionsCommon::validaDataArray($data,'informacao_complementar');
+            $contrato->DescModalidade = ActionsCommon::validaDataArray($data,'modalidade');
+            $contrato->NumLicitacao = ActionsCommon::validaDataArray($data,'licitacao_numero');
+            $contrato->DatAssinatura = ActionsCommon::validaDataArray($data,'data_assinatura');
+            $contrato->DatPublicacao = ActionsCommon::validaDataArray($data,'data_publicacao');
+            $contrato->DatVigenciaInicio = ActionsCommon::validaDataArray($data,'vigencia_inicio');
+            $contrato->DatVigenciaFim = ActionsCommon::validaDataArray($data,'vigencia_fim');
+            $contrato->ValInicial = str_replace(['.', ','], ['', '.'], ActionsCommon::validaDataArray($data,'valor_inicial'));
+            $contrato->ValGlobal = str_replace(['.', ','], ['', '.'], ActionsCommon::validaDataArray($data,'valor_global'));
+            $contrato->NumParcelas = ActionsCommon::validaDataArray($data,'num_parcelas');
+            $contrato->ValParcela = str_replace(['.', ','], ['', '.'], ActionsCommon::validaDataArray($data,'valor_parcela'));
+            $contrato->ValAcumulado = str_replace(['.', ','], ['', '.'], ActionsCommon::validaDataArray($data,'valor_acumulado'));
 
             $contrato->save();
 
