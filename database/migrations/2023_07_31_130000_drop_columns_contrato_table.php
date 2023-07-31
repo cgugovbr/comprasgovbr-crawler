ba17c61aec464ecf623ebc0ed05eee66357e49ef
Evert Ramos <evert.ramos@cgu.gov.br>
2023-07-31 13:25:16 -0300

    Remove colunas de links

---
+++ database/migrations/2023_07_31_130000_drop_columns_contrato_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use App\Schemas\Grammars\CustomGrammar;
+use Illuminate\Database\Schema\Blueprint;
+use App\Schemas\Blueprints\CustomBlueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class DropColumnsContratoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (Schema::hasColumns('Contrato', ['EndLinkHistorico', 'EndLinkEmpenhos', 'EndLinkCronograma'])) {
+
+            Schema::table('Contrato', function (Blueprint $table) {
+                $table->dropcolumn('EndLinkHistorico');
+                $table->dropcolumn('EndLinkEmpenhos');
+                $table->dropcolumn('EndLinkCronograma');
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::connection()->setSchemaGrammar(new CustomGrammar());
+        $schema = DB::connection()->getSchemaBuilder();
+
+        $schema->blueprintResolver(function($table, $callback) {
+            return new CustomBlueprint($table, $callback);
+        });
+
+        $schema->table('Contrato', function (Blueprint $table) {
+            if (!Schema::hasColumns('Contrato', ['EndLinkHistorico', 'End/LinkEmpenhos', 'EndLinkCronograma'])) {
+                $table->varChar('EndLinkHistorico')->nullable();
+                $table->varChar('EndLinkEmpenhos')->nullable();
+                $table->varChar('EndLinkCronograma')->nullable();
+            }
+        });
+    }
+}
