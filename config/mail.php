ec3b0eeef1e33fe3200c24719507b81bcc5a26d5
Evert Ramos <evert.ramos@cgu.gov.br>
2023-08-02 18:46:26 -0300

    Ajustar configuração de email

--- config/mail.php
+++ config/mail.php
@@ -43,6 +43,7 @@ return [
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
             'local_domain' => env('MAIL_EHLO_DOMAIN'),
+            'verify_peer' => false,
         ],
 
         'ses' => [
